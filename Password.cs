83a36a981de0b9351502348bbe19d6d388860088
OleksPal <oleksandr.palamarchuk1310@gmail.com>
2023-09-11 15:17:24 +0300

    Fixed accessability of fields and increment operator

--- Password.cs
+++ Password.cs
@@ -2,7 +2,7 @@
 {
     public abstract class Password
     {
-        protected IPasswordText text;
+        public IPasswordText text;
         protected IAlphabet alphabet;
         protected IOrder orderBy;
          
@@ -17,32 +17,5 @@
             if (!String.IsNullOrEmpty(startPosition))
                 text!.SetStart(startPosition, alphabet);
         }
-
-        public static Password operator ++(Password previousPassword)
-        {
-            LinkedListNode<char>? currentNode = previousPassword.text.Text.Last;
-            // Index of node’s value in alphabet
-            int alphabetIndex = previousPassword.alphabet.Alphabet.IndexOf(currentNode!.Value);
-
-            if (alphabetIndex < previousPassword.alphabet.Alphabet.Length - 1)
-                currentNode.Value = previousPassword.alphabet.Alphabet[++alphabetIndex];
-            else
-            {
-                while (currentNode != null)
-                {
-                    if (currentNode.Previous == null) // Can’t increase previous value
-                        return previousPassword;
-                    currentNode.Value = previousPassword.alphabet.Alphabet[0];
-                    currentNode = currentNode.Previous;
-                    alphabetIndex = previousPassword.alphabet.Alphabet.IndexOf(currentNode.Value);
-                    if (alphabetIndex < previousPassword.alphabet.Alphabet.Length - 1)
-                    {
-                        currentNode.Value = previousPassword.alphabet.Alphabet[++alphabetIndex];
-                        break;
-                    }
-                }
-            }
-            return previousPassword;
-        }        
     }
 }
--- PasswordGenerator.cs
+++ PasswordGenerator.cs
@@ -5,6 +5,36 @@
         public PasswordGenerator(IPasswordText numberOfSymbols, IAlphabet alphabet, IOrder orderBy,
             string? startPosition = null) 
             : base(numberOfSymbols, alphabet, orderBy, startPosition)
-        { }  
+        { }
+
+        public static PasswordGenerator operator ++(PasswordGenerator previousPassword)
+        {
+            PasswordGenerator newPassword = new PasswordGenerator(previousPassword.text,
+                previousPassword.alphabet, previousPassword.orderBy);
+
+            LinkedListNode<char>? currentNode = newPassword.text.Text.Last;
+            // Index of node’s value in alphabet
+            int alphabetIndex = newPassword.alphabet.Alphabet.IndexOf(currentNode!.Value);
+
+            if (alphabetIndex < newPassword.alphabet.Alphabet.Length - 1)
+                currentNode.Value = newPassword.alphabet.Alphabet[++alphabetIndex];
+            else
+            {
+                while (currentNode != null)
+                {
+                    if (currentNode.Previous == null) // Can’t increase previous value
+                        return newPassword;
+                    currentNode.Value = newPassword.alphabet.Alphabet[0];
+                    currentNode = currentNode.Previous;
+                    alphabetIndex = newPassword.alphabet.Alphabet.IndexOf(currentNode.Value);
+                    if (alphabetIndex < newPassword.alphabet.Alphabet.Length - 1)
+                    {
+                        currentNode.Value = newPassword.alphabet.Alphabet[++alphabetIndex];
+                        break;
+                    }
+                }
+            }            
+            return newPassword;
+        }
     }
 }
